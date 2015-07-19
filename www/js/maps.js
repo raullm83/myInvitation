8145ad5588e712f357816ba9f18bc127fe34fb70
raullm83 <raullm.83@gmail.com>
2015-07-19 20:32:50 +0200

    :space:invader subida

---
+++ www/js/maps.js
@@ -0,0 +1,64 @@
+var map;
+var geocoder;
+function initialize() {
+  var mapOptions = {
+    zoom: 15,
+    mapTypeId: google.maps.MapTypeId.SATELLITE
+  };
+  geocoder = new google.maps.Geocoder();
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+  
+  geocoder.geocode( { 'address' : 'Costa ballena, rota'}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      map.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+  
+  // Try HTML5 geolocation
+  /*if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = new google.maps.LatLng(position.coords.latitude,
+                                       position.coords.longitude);
+
+      var infowindow = new google.maps.InfoWindow({
+        map: map,
+        position: pos,
+        content: 'Location found using HTML5.'
+      });
+
+      map.setCenter(pos);
+    }, function() {
+      handleNoGeolocation(true);
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    map.setCenter(myLatlng);
+    handleNoGeolocation(false);
+  }*/
+}
+
+function handleNoGeolocation(errorFlag) {
+  if (errorFlag) {
+    var content = 'Error: The Geolocation service failed.';
+  } else {
+    var content = 'Error: Your browser doesn\'t support geolocation.';
+  }
+
+  var options = {
+    map: map,
+    position: new google.maps.LatLng(60, 105),
+    content: content
+  };
+
+  var infowindow = new google.maps.InfoWindow(options);
+  map.setCenter(options.position);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
